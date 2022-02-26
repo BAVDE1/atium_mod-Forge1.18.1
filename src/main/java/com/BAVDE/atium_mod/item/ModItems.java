2f0098846ed5fa9f651fd07d67a642bcefc42f2f
BryceVE <0362729@schoolsnet.act.edu.au>
2022-02-27 09:23:01 +1100

    changed item to pure atium, added raw atium item

--- src/main/java/com/BAVDE/atium_mod/item/ModItems.java
+++ src/main/java/com/BAVDE/atium_mod/item/ModItems.java
@@ -1,6 +1,6 @@
-package com.BAVDE.testmod.item;
+package com.BAVDE.atium_mod.item;
 
-import com.BAVDE.testmod.TestMod;
+import com.BAVDE.atium_mod.TestMod;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -12,7 +12,10 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS=
             DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MOD_ID);
 
-    public static final RegistryObject<Item> CITRINE = ITEMS.register("citrine",
+    public static final RegistryObject<Item> PUREATIUM = ITEMS.register("pure_atium",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> RAWATIUM = ITEMS.register("raw_atium",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static void register(IEventBus eventBus) {
