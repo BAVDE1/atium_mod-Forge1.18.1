ef1680b3c0039f42cadee7675a3f6188797b285f
BryceVE <0362729@schoolsnet.act.edu.au>
2022-03-02 19:46:16 +1100

    Commented in StoneHammer file

--- src/main/java/com/BAVDE/atium_mod/item/custom/StoneHammer.java
+++ src/main/java/com/BAVDE/atium_mod/item/custom/StoneHammer.java
@@ -2,12 +2,15 @@ package com.BAVDE.atium_mod.item.custom;
 
 import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import org.checkerframework.checker.nullness.qual.Nullable;
+import net.minecraft.world.level.block.BucketPickup;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Consumer;
 
@@ -23,13 +26,15 @@ public class StoneHammer extends Item {
 
     @Override
     public ItemStack getContainerItem(ItemStack itemStack) {
-
-        if (itemStack.getDamageValue() < 16) {
+        if (itemStack.getDamageValue() < itemStack.getMaxDamage()) { //if the durability damage is higher than 16 (if the item has taken less than 16 damage)
             itemStack.setDamageValue(itemStack.getDamageValue() + 1); //item takes 1 durability damage when used in crafting
             return itemStack.copy(); //puts the newly damaged item in the crafting table
         } else {
             itemStack.setDamageValue(itemStack.getDamageValue() + 1); //item takes 1 durability damage when used in crafting
-            return itemStack;
+
+
+
+            return itemStack; //doesn't give item back after crafting because it is NULL
         }
     }
 }
