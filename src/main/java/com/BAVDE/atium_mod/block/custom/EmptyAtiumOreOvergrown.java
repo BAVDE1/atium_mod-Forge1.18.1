de8d4a880610cbc7012624f0202f3c81e6c2bf61
BryceVE <0362729@schoolsnet.act.edu.au>
2022-03-06 11:52:45 +1100

    got regeneration working, if a bit fast! save before i break it

--- src/main/java/com/BAVDE/atium_mod/block/custom/EmptyAtiumOreOvergrown.java
+++ src/main/java/com/BAVDE/atium_mod/block/custom/EmptyAtiumOreOvergrown.java
@@ -5,22 +5,33 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 
 import javax.annotation.Nullable;
 import java.util.Random;
 
 public class EmptyAtiumOreOvergrown extends Block {
+    public static final BooleanProperty TICKING = BooleanProperty.create("ticking");
 
     public EmptyAtiumOreOvergrown(Properties p_49795_) {
         super(p_49795_);
+        this.registerDefaultState(this.defaultBlockState().setValue(TICKING, Boolean.valueOf(true)));
     }
 
     @Override
-    public boolean isRandomlyTicking(BlockState pState) {return super.isRandomlyTicking(pState);}
+    public boolean isRandomlyTicking(BlockState pState) {
+        return pState.getValue(TICKING);
+    }
 
     @Override
     public void randomTick(BlockState pState, ServerLevel pLevel, BlockPos pPos, Random pRandom) {
-        pLevel.setBlock(pPos, ModBlocks.ATIUM_ORE_OVERGROWN.get().defaultBlockState(), 3);
-        super.randomTick(pState, pLevel, pPos, pRandom);
+        if (pState.getValue(TICKING)) {
+            pLevel.setBlock(pPos, ModBlocks.ATIUM_ORE_OVERGROWN.get().defaultBlockState(), 3);
+        }
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(TICKING);
     }
 }
