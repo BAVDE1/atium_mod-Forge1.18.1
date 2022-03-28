6e25210bb0104319b9f9992cd5c5e72affd9dc19
BryceVE <0362729@schoolsnet.act.edu.au>
2022-03-28 16:04:44 +1100

    clicked, no functionality

--- src/main/java/com/BAVDE/atium_mod/block/custom/BuddingCrystallineLeaves.java
+++ src/main/java/com/BAVDE/atium_mod/block/custom/BuddingCrystallineLeaves.java
@@ -1,15 +1,24 @@
 package com.BAVDE.atium_mod.block.custom;
 
+import net.minecraft.client.resources.sounds.Sound;
+import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.client.event.sound.SoundEvent;
+import org.lwjgl.system.CallbackI;
 
 import java.util.Random;
 
@@ -45,6 +54,21 @@ public class BuddingCrystallineLeaves extends LeavesBlock {
 
         if (growth < 10) { //if growth less than 10
             pLevel.setBlock(pPos, pState.setValue(GROWTH, (growth + 1)), 3); //add 1 to growth state
+        }
+    }
+
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        if(!pLevel.isClientSide() && pHand == InteractionHand.MAIN_HAND) {
+            if (pState.getValue(GROWTH) == 10) {
+                pLevel.setBlock(pPos, pState.setValue(GROWTH, 0), 3);
+                //pLevel.playSound(pPlayer, pPos, SoundEvent.);
+                return InteractionResult.SUCCESS;
+            } else {
+                return InteractionResult.PASS;
+            }
+        } else {
+            return InteractionResult.PASS;
         }
     }
 
