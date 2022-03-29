fd40d15244586d18cddec7ffffa7aeecb4103c65
BryceVE <0362729@schoolsnet.act.edu.au>
2022-03-29 12:56:29 +1100

    added the logs and planks to their tags, commented BuddingCrystallineLeaves code

--- src/main/java/com/BAVDE/atium_mod/block/custom/BuddingCrystallineLeaves.java
+++ src/main/java/com/BAVDE/atium_mod/block/custom/BuddingCrystallineLeaves.java
@@ -51,7 +51,7 @@ public class BuddingCrystallineLeaves extends LeavesBlock {
     @Override
     public boolean isRandomlyTicking(BlockState pState) {
         return true;
-    }
+    } //always ticking
 
     @Override
     public void randomTick(BlockState pState, ServerLevel pLevel, BlockPos pPos, Random pRandom) {
@@ -65,21 +65,22 @@ public class BuddingCrystallineLeaves extends LeavesBlock {
 
         if (growth < 9) { //if growth less than 10
             pLevel.setBlock(pPos, pState.setValue(GROWTH, (growth + 1)), 3); //add 1 to growth state
-        } else if (growth == 9) {
-            pLevel.setBlock(pPos, pState.setValue(GROWTH, 10).setValue(GROWN, true), 3);
+        } else if (growth == 9) { //if growth is 9
+            pLevel.setBlock(pPos, pState.setValue(GROWTH, 10).setValue(GROWN, true), 3); //sets growth to 10 and growth to true
         }
     }
 
     @Override
     public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
-        ItemStack itemStack = pPlayer.getItemInHand(pHand);
+        ItemStack itemStack = pPlayer.getItemInHand(pHand); //gets what the user is holding
+        //if not client side and holding nothing or crystallized shard when right-clicked
         if(!pLevel.isClientSide() && (itemStack.isEmpty()) || (itemStack.is(ModItems.CRYSTALLIZED_SHARD.get()))) {
-            if (pState.getValue(GROWN)) {
-                pLevel.setBlock(pPos, pState.setValue(GROWTH, 0).setValue(GROWN, false), 3);
-                pLevel.playSound((Player)null, pPos, SoundEvents.AMETHYST_BLOCK_HIT, SoundSource.BLOCKS, 1.0F, 0.5F + pLevel.random.nextFloat() * 1.2F);
-                pLevel.playSound((Player)null, pPos, SoundEvents.AMETHYST_BLOCK_CHIME, SoundSource.BLOCKS, 4.0F, 0.5F + pLevel.random.nextFloat() * 1.2F);
-                int amount = 1 + this.RANDOM.nextInt(3);
-                popResource(pLevel, pPos, new ItemStack(ModItems.CRYSTALLIZED_SHARD.get(), amount));
+            if (pState.getValue(GROWN)) { //if the blocks is fully grown (true)
+                pLevel.setBlock(pPos, pState.setValue(GROWTH, 0).setValue(GROWN, false), 3); //set growth to 0 and grown to false
+                pLevel.playSound((Player)null, pPos, SoundEvents.AMETHYST_BLOCK_HIT, SoundSource.BLOCKS, 1.0F, 0.5F + pLevel.random.nextFloat() * 1.2F); //plays sound
+                pLevel.playSound((Player)null, pPos, SoundEvents.AMETHYST_BLOCK_CHIME, SoundSource.BLOCKS, 4.0F, 0.5F + pLevel.random.nextFloat() * 1.2F); //plays sound
+                int amount = 1 + this.RANDOM.nextInt(3); //generates random number between 1 and 3
+                popResource(pLevel, pPos, new ItemStack(ModItems.CRYSTALLIZED_SHARD.get(), amount)); //drops 1 to 3 crystallized shards
                 return InteractionResult.SUCCESS;
             } else {
                 return InteractionResult.PASS;
