21599387fe2edb6f3f1e7ec3ddbf78337e913226
BryceVE <0362729@schoolsnet.act.edu.au>
2022-04-22 17:55:25 +1000

    infusing table crafting is half working; result changes on ingredients slots change but doesnt consume ingredients on craft

--- src/main/java/com/BAVDE/atium_mod/screen/InfusingTableMenu.java
+++ src/main/java/com/BAVDE/atium_mod/screen/InfusingTableMenu.java
@@ -4,6 +4,7 @@ import com.BAVDE.atium_mod.block.ModBlocks;
 import com.BAVDE.atium_mod.block.entity.InfusingTableBlockEntity;
 import com.BAVDE.atium_mod.item.ModItems;
 import com.BAVDE.atium_mod.recipe.InfusingTableRecipe;
+import com.BAVDE.atium_mod.recipe.ModRecipes;
 import com.BAVDE.atium_mod.screen.slot.ModInputSlot;
 import com.BAVDE.atium_mod.screen.slot.ModResultSlot;
 import net.minecraft.network.FriendlyByteBuf;
@@ -14,40 +15,35 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.*;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.crafting.UpgradeRecipe;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
 import net.minecraftforge.items.SlotItemHandler;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Optional;
 
 public class InfusingTableMenu extends AbstractInfusingMenu {
     private final InfusingTableBlockEntity blockEntity;
     private final List<InfusingTableRecipe> recipes;
     public final Level level;
     protected final Player player;
+    protected final ContainerLevelAccess access;
 
-    @Override
-    protected boolean mayPickup(Player player, boolean bool) {
-        return true;
-    }
-    @Override
-    protected void onTake(Player player, ItemStack itemStack) {
-    }
-    @Override
-    protected boolean isValidBlock(BlockState blockState) {
-        return true;
-    }
 
     public InfusingTableMenu(int window, Inventory inv, FriendlyByteBuf friendlyByteBuf) {
-        this(window, inv, inv.player.level.getBlockEntity(friendlyByteBuf.readBlockPos()));
+        this(window, inv, inv.player.level.getBlockEntity(friendlyByteBuf.readBlockPos()), ContainerLevelAccess.NULL);
     }
 
-    public InfusingTableMenu(int window, Inventory inv, BlockEntity entity) {
+    public InfusingTableMenu(int window, Inventory inv, BlockEntity entity, ContainerLevelAccess access) {
         super(ModMenuTypes.INFUSING_TABLE_MENU.get(), window, inv, entity);
         blockEntity = ((InfusingTableBlockEntity) entity);
+        this.access = access;
         this.player = inv.player;
         this.level = inv.player.level;
         this.recipes = this.level.getRecipeManager().getAllRecipesFor(InfusingTableRecipe.Type.INSTANCE);
@@ -79,6 +75,61 @@ public class InfusingTableMenu extends AbstractInfusingMenu {
         });
         addPlayerInventory(inv, 139);
         addPlayerHotbar(inv, 196);
+    }
+
+    @Override
+    protected boolean mayPickup(Player player, boolean bool) {
+        return true;
+    }
+
+    @Override
+    protected void onTake(Player player, ItemStack itemStack) {
+        itemStack.onCraftedBy(player.level, player, itemStack.getCount());
+        this.shrinkStackInSlot(0);
+        this.shrinkStackInSlot(1);
+        this.access.execute((level, blockPos) -> {
+            level.levelEvent(1044, blockPos, 0);
+        });
+    }
+
+    private void shrinkStackInSlot(int slot) {
+        ItemStack itemstack = blockEntity.itemHandler.getStackInSlot(slot);
+        itemstack.shrink(1);
+        blockEntity.itemHandler.setStackInSlot(slot, itemstack);
+    }
+
+    @Override
+    protected boolean isValidBlock(BlockState blockState) {
+        return true;
+    }
+
+    @Override
+    public void createResult() {
+        if (hasRecipe(blockEntity)) {
+            Level level = blockEntity.getLevel();
+            SimpleContainer inventory = new SimpleContainer(blockEntity.itemHandler.getSlots());
+            for (int i = 0; i < blockEntity.itemHandler.getSlots(); i++) {
+                inventory.setItem(i, blockEntity.itemHandler.getStackInSlot(i));
+            }
+            Optional<InfusingTableRecipe> match = level.getRecipeManager().getRecipeFor(InfusingTableRecipe.Type.INSTANCE, inventory, level);
+
+            if (match.isPresent()) {
+                blockEntity.itemHandler.setStackInSlot(2, new ItemStack(match.get().getResultItem().getItem(), 1));
+            }
+        } else {
+            blockEntity.itemHandler.setStackInSlot(2, ItemStack.EMPTY);
+        }
+    }
+
+    private static boolean hasRecipe(InfusingTableBlockEntity entity) {
+        Level level = entity.getLevel();
+        SimpleContainer inventory = new SimpleContainer(entity.itemHandler.getSlots());
+        for (int i = 0; i < entity.itemHandler.getSlots(); i++) {
+            inventory.setItem(i, entity.itemHandler.getStackInSlot(i));
+        }
+        Optional<InfusingTableRecipe> match = level.getRecipeManager().getRecipeFor(InfusingTableRecipe.Type.INSTANCE, inventory, level);
+
+        return match.isPresent();
     }
 
     public int hasMetal() {
