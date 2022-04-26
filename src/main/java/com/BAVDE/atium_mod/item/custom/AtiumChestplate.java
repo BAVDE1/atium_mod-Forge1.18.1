a800cde4d1f45b9f830327aefda6f8147f94df20
BryceVE <0362729@schoolsnet.act.edu.au>
2022-04-26 13:01:21 +1000

    added infused chestplates tooltip functionality; added infusing recipes for helmet, leggings, boots;

--- src/main/java/com/BAVDE/atium_mod/item/custom/AtiumChestplate.java
+++ src/main/java/com/BAVDE/atium_mod/item/custom/AtiumChestplate.java
@@ -1,34 +1,72 @@
 package com.BAVDE.atium_mod.item.custom;
 
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.item.ArmorMaterial;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
-import org.stringtemplate.v4.ST;
 
 import javax.annotation.Nullable;
 import java.util.List;
 
 public class AtiumChestplate extends ArmorItem {
+
     public AtiumChestplate(ArmorMaterial pMaterial, EquipmentSlot pSlot, Properties pProperties) {
         super(pMaterial, pSlot, pProperties);
     }
 
     @Override
-    public boolean isFoil(ItemStack pStack) {
-        return (pStack.getTag().contains("atium_mod.metal") || pStack.isEnchanted());
+    public Rarity getRarity(ItemStack pStack) {
+        if (!pStack.isEnchanted() || pStack.getTag().contains("atium_mod.metal")) {
+            return this.rarity;
+        } else {
+            switch(this.rarity) {
+                case COMMON:
+                case UNCOMMON:
+                    return Rarity.RARE;
+                case RARE:
+                    return Rarity.EPIC;
+                case EPIC:
+                default:
+                    return this.rarity;
+            }
+        }
     }
 
     @Override
     public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
         if(pStack.getTag().contains("atium_mod.metal")) {
-            String currentMetal = pStack.getTag().getString("atium_mod.metal");
-            pTooltipComponents.add(new TextComponent("trol"));
+            int currentMetal = pStack.getTag().getInt("atium_mod.metal");
+            if (Screen.hasShiftDown()) {
+                switch (currentMetal) { //1=iron, 2=steel, 3=tin, 4=pewter, 5=brass, 6=zinc, 7=copper, 8=bronze, 9=gold
+                    case 1 -> pTooltipComponents.add(new TranslatableComponent("tooltip.atium_mod.atium_chestplate.tooltip.iron.shift"));
+                    case 2 -> pTooltipComponents.add(new TranslatableComponent("tooltip.atium_mod.atium_chestplate.tooltip.steel.shift"));
+                    case 3 -> pTooltipComponents.add(new TranslatableComponent("tooltip.atium_mod.atium_chestplate.tooltip.tin.shift"));
+                    case 4 -> pTooltipComponents.add(new TranslatableComponent("tooltip.atium_mod.atium_chestplate.tooltip.pewter.shift"));
+                    case 5 -> pTooltipComponents.add(new TranslatableComponent("tooltip.atium_mod.atium_chestplate.tooltip.brass.shift"));
+                    case 6 -> pTooltipComponents.add(new TranslatableComponent("tooltip.atium_mod.atium_chestplate.tooltip.zinc.shift"));
+                    case 9 -> pTooltipComponents.add(new TranslatableComponent("tooltip.atium_mod.atium_chestplate.tooltip.gold.shift"));
+                }
+            } else {
+                switch (currentMetal) { //1=iron, 2=steel, 3=tin, 4=pewter, 5=brass, 6=zinc, 7=copper, 8=bronze, 9=gold
+                    case 1 -> pTooltipComponents.add(new TranslatableComponent("tooltip.atium_mod.atium_chestplate.tooltip.iron"));
+                    case 2 -> pTooltipComponents.add(new TranslatableComponent("tooltip.atium_mod.atium_chestplate.tooltip.steel"));
+                    case 3 -> pTooltipComponents.add(new TranslatableComponent("tooltip.atium_mod.atium_chestplate.tooltip.tin"));
+                    case 4 -> pTooltipComponents.add(new TranslatableComponent("tooltip.atium_mod.atium_chestplate.tooltip.pewter"));
+                    case 5 -> pTooltipComponents.add(new TranslatableComponent("tooltip.atium_mod.atium_chestplate.tooltip.brass"));
+                    case 6 -> pTooltipComponents.add(new TranslatableComponent("tooltip.atium_mod.atium_chestplate.tooltip.zinc"));
+                    case 7 -> pTooltipComponents.add(new TranslatableComponent("tooltip.atium_mod.atium_chestplate.tooltip.copper"));
+                    case 8 -> pTooltipComponents.add(new TranslatableComponent("tooltip.atium_mod.atium_chestplate.tooltip.bronze"));
+                    case 9 -> pTooltipComponents.add(new TranslatableComponent("tooltip.atium_mod.atium_chestplate.tooltip.gold"));
+                }
+            }
         }
         super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
+
+    private void gold() {
+
     }
 }
