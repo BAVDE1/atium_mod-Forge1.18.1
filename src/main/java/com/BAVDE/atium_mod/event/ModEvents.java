bef584020aee711fab162638581973b887e1a95d
BryceVE <0362729@schoolsnet.act.edu.au>
2022-04-27 20:49:09 +1000

    added sound fx to the test infusion

--- src/main/java/com/BAVDE/atium_mod/event/ModEvents.java
+++ src/main/java/com/BAVDE/atium_mod/event/ModEvents.java
@@ -1,9 +1,7 @@
 package com.BAVDE.atium_mod.event;
 
 import com.BAVDE.atium_mod.AtiumMod;
-import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
-import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -11,7 +9,7 @@ import net.minecraftforge.fml.common.Mod;
 public class ModEvents {
 
     @SubscribeEvent
-    public static void resetFreeze(ClientPlayerNetworkEvent.RespawnEvent respawnEvent) {
+    public static void resetFreezeOnRespawn(ClientPlayerNetworkEvent.RespawnEvent respawnEvent) {
         respawnEvent.getNewPlayer().setTicksFrozen(0);
     }
 }
