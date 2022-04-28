db157d45111265f7292cb3a6b4e8087f5c4bc04a
BryceVE <0362729@schoolsnet.act.edu.au>
2022-04-28 10:57:14 +1000

    messing with particles

--- src/main/java/com/BAVDE/atium_mod/item/custom/AtiumSword.java
+++ src/main/java/com/BAVDE/atium_mod/item/custom/AtiumSword.java
@@ -1,9 +1,13 @@
 package com.BAVDE.atium_mod.item.custom;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.particles.ParticleType;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
@@ -15,15 +19,15 @@ public class AtiumSword extends SwordItem {
 
     @Override
     public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
-        int chance = pTarget.level.random.nextInt(100);
+        //int chance = pTarget.level.random.nextInt(100);
 
-        if (chance > 50) {
+        //if (chance > 50) {
             pTarget.setTicksFrozen(139);
             if (!pTarget.hasEffect(MobEffects.MOVEMENT_SLOWDOWN)) {
-                pTarget.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 20, 3), pAttacker);
+                pTarget.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 80, 3), pAttacker);
             }
-            pTarget.playSound(SoundEvents.SKELETON_CONVERTED_TO_STRAY, 2.0F, 1.0F);
-        }
+            pTarget.playSound(SoundEvents.SKELETON_CONVERTED_TO_STRAY, 4.0F, 1.0F);
+        //}
         return super.hurtEnemy(pStack, pTarget, pAttacker);
     }
 }
