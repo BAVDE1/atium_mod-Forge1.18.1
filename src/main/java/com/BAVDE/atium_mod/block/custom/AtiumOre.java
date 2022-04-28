568aecb5de933e59a20273f8a1d4ce83efd3826d
BryceVE <0362729@schoolsnet.act.edu.au>
2022-04-28 10:57:14 +1000

    messing with particles

--- src/main/java/com/BAVDE/atium_mod/block/custom/AtiumOre.java
+++ src/main/java/com/BAVDE/atium_mod/block/custom/AtiumOre.java
@@ -2,6 +2,7 @@ package com.BAVDE.atium_mod.block.custom;
 
 import com.BAVDE.atium_mod.block.ModBlocks;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -9,6 +10,8 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Random;
 
 public class AtiumOre extends Block {
     public AtiumOre(Properties p_49795_) {
@@ -20,5 +23,20 @@ public class AtiumOre extends Block {
         super.playerDestroy(pLevel, pPlayer, pPos, pState, pBlockEntity, pTool);
         //when block is destroyed replace with empty atium ore
         pLevel.setBlock(pPos, ModBlocks.EMPTY_ATIUM_ORE.get().defaultBlockState(), 3);
+    }
+
+    @Override
+    public void animateTick(BlockState pState, Level pLevel, BlockPos pPos, Random pRandom) {
+        super.animateTick(pState, pLevel, pPos, pRandom);
+        float chance = 0.5f;
+        if (chance < pRandom.nextFloat()) {
+            pLevel.addParticle(ParticleTypes.EXPLOSION_EMITTER, pPos.getX(), pPos.getY(), pPos.getZ(), 1.0D, 0.0D, 0.0D);
+            pLevel.addParticle(ParticleTypes.EXPLOSION, pPos.getX(), pPos.getY(), pPos.getZ(), 1.0D, 0.0D, 0.0D);
+
+            /*for (int i = 0; i < 10; i++) {
+                pLevel.addParticle(ParticleTypes.ENCHANTED_HIT, pPos.getX(), pPos.getY(), pPos.getZ(),
+                        1d, 0d + pRandom.nextDouble(0.05d), 1d);
+            }*/
+        }
     }
 }
