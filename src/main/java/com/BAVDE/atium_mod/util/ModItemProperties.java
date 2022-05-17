499fe8eef9feed56aeba3669c2c0994b09748560
BryceVE <0362729@schoolsnet.act.edu.au>
2022-05-18 09:54:15 +1000

    updated atium compass texture, added idle animation

--- src/main/java/com/BAVDE/atium_mod/util/ModItemProperties.java
+++ src/main/java/com/BAVDE/atium_mod/util/ModItemProperties.java
@@ -8,7 +8,9 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.item.ClampedItemPropertyFunction;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.client.renderer.item.ItemPropertyFunction;
+import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.decoration.ItemFrame;
@@ -16,6 +18,9 @@ import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 
@@ -33,12 +38,13 @@ public class ModItemProperties {
         InfusableItem(ModItems.ATIUM_SHOVEL.get());
         InfusableItem(ModItems.ATIUM_HOE.get());
 
-        ModCompassItem(ModItems.ATIUM_COMPASS.get());
+        ModCompassItemUse(ModItems.ATIUM_COMPASS.get());
+        ModCompassItemWobble(ModItems.ATIUM_COMPASS.get());
     }
 
     static void InfusableItem(Item item) {
         ItemProperties.register(item, new ResourceLocation("metal"), (itemStack, clientLevel, livingEntity, i) -> {
-            Entity entity = (Entity)(livingEntity != null ? livingEntity : itemStack.getEntityRepresentation());
+            Entity entity = (Entity) (livingEntity != null ? livingEntity : itemStack.getEntityRepresentation());
             int metal = 0;
 
             if (entity != null) {
@@ -48,9 +54,69 @@ public class ModItemProperties {
         });
     }
 
-    static void ModCompassItem(Item item) {
+    static void ModCompassItemUse(Item item) {
         ItemProperties.register(item, new ResourceLocation("using"), (itemStack, clientLevel, livingEntity, i) -> {
             return livingEntity != null && AtiumCompass.isUsing(itemStack) ? 1.0F : 0.0F;
         });
+    }
+
+    static void ModCompassItemWobble(Item item) {
+        ItemProperties.register(item, new ResourceLocation("angle"), new ClampedItemPropertyFunction() {
+            private final ModItemProperties.CompassWobble wobbleRandom = new ModItemProperties.CompassWobble();
+
+            @Override
+            public float unclampedCall(ItemStack itemStack, @Nullable ClientLevel clientLevel, @Nullable LivingEntity livingEntity, int pSeed) {
+                Entity entity = (Entity) (livingEntity != null ? livingEntity : itemStack.getEntityRepresentation());
+                if (entity == null) {
+                    return 0.0F;
+                } else {
+                    if (clientLevel == null && entity.level instanceof ClientLevel) {
+                        clientLevel = (ClientLevel) entity.level;
+                    }
+                    long gameTime = clientLevel.getGameTime();
+
+                    //always wobble random atm
+                    if (this.wobbleRandom.shouldUpdate(gameTime)) {
+                        this.wobbleRandom.update(gameTime, Math.random());
+                    }
+                    double d0 = this.wobbleRandom.rotation + (double) ((float) this.hash(pSeed) / 2.14748365E9F);
+                    return Mth.positiveModulo((float) d0, 1.0F);
+                }
+            }
+
+            private int hash(int p_174670_) {
+                return p_174670_ * 1327217883;
+            }
+
+            @javax.annotation.Nullable
+            private BlockPos getStructurePosition(ClientLevel clientLevel) {
+                if (item instanceof AtiumCompass) {
+                    //locate structure here
+                    return clientLevel.dimensionType().natural() ? clientLevel.getSharedSpawnPos() : null;
+                } else {
+                    return null;
+                }
+            }
+        });
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    static class CompassWobble {
+        double rotation;
+        private double deltaRotation;
+        private long lastUpdateTick;
+
+        boolean shouldUpdate(long pGameTime) {
+            return this.lastUpdateTick != pGameTime;
+        }
+
+        void update(long pGameTime, double pWobbleAmount) {
+            this.lastUpdateTick = pGameTime;
+            double d0 = pWobbleAmount - this.rotation;
+            d0 = Mth.positiveModulo(d0 + 0.5D, 1.0D) - 0.5D;
+            this.deltaRotation += d0 * 0.1D;
+            this.deltaRotation *= 0.8D;
+            this.rotation = Mth.positiveModulo(this.rotation + this.deltaRotation, 1.0D);
+        }
     }
 }
